773c8d6d2b6db2995fa0db78c88f44871f195a3f
Arttu Tanner <arttu.tanner@invian.fi>
2014-04-04 23:41:04 +0300

    html and some js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "Voitit partion!" : "Erosit partiosta!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
